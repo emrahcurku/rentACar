64cbd51441a4b31a9cf448807d8c1855a48ef118
emrahcurku <emrah.curku@outlook.com>
2024-08-04 18:21:32 +0300

    brand entities created

---
+++ src/main/java/com/example/rentACar/entities/Brand.java
@@ -0,0 +1,28 @@
+package com.example.rentACar.entities;
+
+public class Brand {
+	
+	private int id;
+	private String name;
+	
+	public Brand() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
