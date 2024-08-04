098d7155c61438758a84d0b1241f35f36c3f584d
emrahcurku <emrah.curku@outlook.com>
2024-08-04 20:36:57 +0300

    constructor deleted

--- src/main/java/com/example/rentACar/entities/Brand.java
+++ src/main/java/com/example/rentACar/entities/Brand.java
@@ -8,12 +8,6 @@ public class Brand {
 	public Brand() {
 		super();
 	}
-	
-	public Brand(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
 
 	public int getId() {
 		return id;
