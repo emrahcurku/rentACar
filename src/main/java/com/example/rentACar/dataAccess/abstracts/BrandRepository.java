f2a179b4b778fd71c7dd5555bb1354cca5f956f6
emrahcurku <emrah.curku@outlook.com>
2024-08-19 01:23:41 +0300

    update

--- src/main/java/com/example/rentACar/dataAccess/abstracts/BrandRepository.java
+++ src/main/java/com/example/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.rentACar.entities.concretes.Brand;
 
 //@Repository annotasyonunu yazmamıza gerek yoktur çünkü spring in içinde olan 
-//jpa repositoryi extend ettiğimiz için spring bunun bir repository olduğunu anlar
+//jpa repositoryi extend ettiğimiz için spring bunun bir repository olduğunu anlar.
 public interface BrandRepository extends JpaRepository<Brand, Integer>{
 	
 	
